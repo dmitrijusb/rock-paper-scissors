8ab9cb65c8da7673f0d05fad7f5f7d6639c14d31
Dmitrijus <dmitrijus.byckovas@stud.svako.lt>
2022-09-01 12:36:14 +0300

    Added game function and cleaned the final code

--- script/script.js
+++ script/script.js
@@ -1,28 +1,46 @@
+// Function to get random choice out of 3
 function getComputerChoice() {
   let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
-
+// Function to play one round
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
-  // Testing purpose
   console.log(playerSelection);
   console.log(computerSelection);
 
-  if (playerSelection == computerSelection) return false;
-  else if (
+  if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   )
     return true;
-  return false;
 }
 
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// Function to Run 5 games and count wins
+function game() {
+  let totalGames = 5;
+  let counter = 0;
+  console.log("Welcome, let's play 5 games!");
+
+  for (let index = 0; index < totalGames; index++) {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Please enter your selection");
+    if (playRound(playerSelection, computerSelection)) {
+      counter++;
+    }
+  }
+
+  if (counter > 0)
+    console.log(
+      `Congratulations, you've won ${counter} out of ${totalGames} games!`
+    );
+  else {
+    console.log(`You've lost all 5 games!`);
+  }
+}
+
+game();
