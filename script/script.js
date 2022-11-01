df1a5469c3c4c6fe851a0372770265ba092ff477
Dmitrijus <dmitrijus.byckovas@stud.svako.lt>
2022-11-01 16:05:15 +0200

    Finished modifying playround function

--- script/script.js
+++ script/script.js
@@ -1,33 +1,58 @@
+///////////////////////////////////////////////////
+//
+//
+//
+//
+// //Variables
+const choices = ["Rock", "Paper", "Scissors"];
+let scores = { player: 0, computer: 0 };
+console.log(scores.player);
+
+const [playerImage, computerImage] = document.querySelectorAll(".images");
+const [playerScoreText, computerScoreText] =
+  document.querySelectorAll(".score");
+const buttons = document.querySelectorAll("button");
+let textBox = document.querySelector(".game-text");
+
 // Function to get random choice out of 3
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 // Function to play one round
-function playRound(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  computerSelection =
-    computerSelection[0].toUpperCase() +
-    computerSelection.slice(1).toLowerCase();
+function playRound(e) {
+  const computerSelection = getComputerChoice();
+  const playerSelection = e.target.textContent;
 
   // Player wins logic
   if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper")
-  )
-    return `You Won! ${playerSelection} beats ${computerSelection}`;
+  ) {
+    textBox.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
+    scores.player += 1;
+    playerScoreText.textContent = scores.player;
+  }
+
   // Computer wins logic
   else if (
     (computerSelection == "Rock" && playerSelection == "Scissors") ||
     (computerSelection == "Paper" && playerSelection == "Rock") ||
     (computerSelection == "Scissors" && playerSelection == "Paper")
-  )
-    return `You Lost! ${computerSelection} beats ${playerSelection}`;
-  else return "It's a Tie!";
+  ) {
+    textBox.textContent = `You Lost! ${computerSelection} beats ${playerSelection}`;
+    scores.computer += 1;
+    computerScoreText.textContent = scores.computer;
+  } else textBox.textContent = "It's a Tie!";
+
+  //Showing images for each choice
+  playerImage.classList.add("mirror-image");
+  playerImage.src = `images/${playerSelection}.svg`;
+  computerImage.src = `images/${computerSelection}.svg`;
 }
+
+//Function to check the winner
 
 // Function to Run 5 games and count wins
 function game() {
@@ -60,28 +85,9 @@ function game() {
 ////////////////////////DOM///////////////////////////
 //////////////////////////////////////////////////////
 
-// //Variables
-// const image = document.querySelectorAll(".images");
+// We can loop through queryselectorall as an array
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", playRound);
+}
 
-// //console.log(playRound("Paper", getComputerChoice()));
-// const buttons = document.querySelectorAll("button");
-// // We can loop through queryselectorall as an array
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", playersChoice);
-// }
-
-// //srcElement
-// // :
-// // button.btn.btn--paper
-
-// function playersChoice(e) {
-//   const computerChoice = getComputerChoice();
-//   const playerChoice = e.target.textContent;
-
-//   document.querySelector(".game-text").textContent = playRound(
-//     playerChoice,
-//     computerChoice
-//   );
-//   image[0].src = `images/${playerChoice}.svg`;
-//   image[1].src = `images/${computerChoice}.svg`;
-// }
+function init() {}
